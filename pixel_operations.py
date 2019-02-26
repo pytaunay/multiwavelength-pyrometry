c005e9766930f113fa858efaaf46cb5a7d92da03
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-02-26 16:47:03 -0500

    Create subroutines to calculate the pair of pixels used and chose pixels to use

---
+++ pixel_operations.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2019 Pierre-Yves Taunay
+# 
+# This program is free software: you can redistribute it andor modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https:/www.gnu.org/licenses/>.
+# 
+# Contact info: https:/github.com/pytaunay
+# 
+# Source: https:/github.com/pytaunay/ILX526A
+
+
+import numpy as np
+
+from spectropyrometer_constants import pix_slice
+
+''' Function: choose_pixels
+Finds the pixels from which we will compute the "two-wavelengths" temperatures
+Inputs:
+    - pix_vec: vector of pixels with which we work
+    - bin_method: the method by which a pixel is picked from a bin. "Average"
+    uses the average value of the boundaries of the pixel bin. "Random" picks
+    a random pixel within the boundaries of the pixel bin
+Outputs:
+    - chosen_pix: vector of chosen pixels
+'''
+def choose_pixels(pix_vec,bin_method='average'):
+    ### Bin the pixels
+    bins = pix_vec[0::pix_slice]
+    
+    ### For each bin, find a pixel
+    chosen_pix = []
+    for idx in bins[:-1]:
+        # Find the low and high bounds of that slice
+        lo = pix_vec[idx]
+        hi = pix_vec[idx + pix_slice - 1]
+        
+        # Pick a pixel in the bin
+        # Can be the average of the boundaries of the bin or a random pixel
+        if bin_method == 'average':
+            pix_idx = (int)(np.average([lo,hi]))
+        elif bin_method == 'random':
+            pix_idx = np.random.choice(pix_vec[lo:hi],size=1)[0]
+        
+        
+        chosen_pix.append(pix_idx)
+    chosen_pix = np.array(chosen_pix)  
+
+    return chosen_pix
+
+
+'''
+Function: generate_combinations
+Generates the pixel combinations that are used to compute the estimated 
+"two-wavelengths" temperature
+Inputs:
+    - pix_vec: the vector of pixels with which we work
+    - bin_method: the method by which a pixel is picked from a bin. "Average"
+    uses the average value of the boundaries of the pixel bin. "Random" picks
+    a random pixel within the boundaries of the pixel bin
+Outputs:
+    - cmb_pix: the array of pixel combinations    
+'''
+def generate_combinations(pix_vec,bin_method='average'):
+  
+    chosen_pix = choose_pixels(pix_vec,bin_method)    
+    
+    cmb_pix = []
+
+    # For each pixel p0 we picked...
+    for p0 in chosen_pix:
+        
+        # Get corresponding pair pixel above and below this pixel p0
+        # They belong to other slices
+        p1vec_p = pix_vec[p0::pix_slice]
+        p1vec_m = pix_vec[p0::-pix_slice]
+        
+        # Create a vector of pixels, remove any duplicates, make sure we do not
+        # include p0
+        p1vec = np.concatenate((p1vec_m,p1vec_p))
+        p1vec = np.unique(p1vec)
+        p1vec = p1vec[p1vec != p0]
+        
+        # Create the list of combinations        
+        for p1 in p1vec:      
+            cmb_pix.append((p0,p1))
+            
+    cmb_pix = np.array(cmb_pix)
+    return cmb_pix
+
+

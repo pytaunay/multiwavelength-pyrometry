9baecd8221314a3e14af412ddf0f39107e0a7188
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-02-26 17:15:59 -0500

    Move generating functions in

--- generate_spectrum.py
+++ generate_spectrum.py
@@ -19,7 +19,9 @@
 
 import numpy as np
 
-from spectropyrometer_constants import C1,C2
+from scipy.interpolate import splrep
+
+from spectropyrometer_constants import C1,C2,window_length
 
 '''
 Function: wien_approximation
@@ -34,3 +36,46 @@ def wien_approximation(wl,T,f_eps):
     eps = f_eps(wl,T) # Emissivity
     
     return eps * C1 / wl**5 * np.exp(-C2/(T*wl))
+
+'''
+Function: generate_artificial_spectrum
+Computes an artificial spectrum with noise
+Inputs:
+    - wl_vec: vector of wavelengths
+    - T: the target temperature
+    - f_eps: the emissivity chosen
+'''
+def generate_data(wl_vec,T,f_eps):
+    # Intensity from Wien's approximation
+    I_calc = wien_approximation(wl_vec,T,f_eps)
+    
+    # Add some noisy and take the log base 10
+    noisy_data = np.random.normal(I_calc,0.1*I_calc)
+    log_noisy = np.log10(noisy_data)
+    
+    # Moving average
+    data_padded = np.pad(log_noisy, 
+                         (window_length//2, window_length-1-window_length//2), 
+                         mode='edge')
+    
+#    # Not so much for lower values; a linear fit to the data is better there
+#    m_dp,b_dp = np.polyfit(wl_vec[window_length:2*window_length],
+#                     data_padded[window_length:2*window_length],1)
+    
+    #data_padded[0:window_length] = m_dp*lnm_vec[0:window_length] + b_dp
+    filtered_data = np.convolve(data_padded, 
+                                np.ones((window_length,))/window_length, 
+                                mode='valid')
+    
+    ### Remove the edge effects
+    wl_vec_sub = wl_vec[window_length:-window_length]
+    filtered_data = filtered_data[window_length:-window_length]
+    pix_vec = pix_vec[window_length:-window_length]
+    
+    
+    ### Fit a line through the noise with some smoothing
+    data_spl = splrep(wl_vec_sub,filtered_data)
+    
+    
+    return I_calc,noisy_data,filtered_data,data_spl
+    

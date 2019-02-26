6a12a0abe800d5f059b4ed99ff6193f1bfeb5bd9
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-02-26 16:27:43 -0500

    Add standard error percentage threshold

--- spectropyrometer_constants.py
+++ spectropyrometer_constants.py
@@ -22,5 +22,8 @@
 C1 = 1.191e16 # W/nm4/cm2 Sr
 C2 = 1.4384e7 # nm K
 
-# Total number of pixels to take into a slice
-pix_slice = 300
+
+pix_slice = 300 # Total number of pixels to take into a slice
+max_poly_order = 5 # Maximum polynomial order to consider
+rse_threshold = 0.5 # Standard error threshold (%) before we consider the fit 
+# to be "bad"

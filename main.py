7402cb10ad0d8e391b5629b98dcaa16dc70e81ef
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-03-19 16:36:13 -0400

    Comment out unnecessary code for now and remove functions that do not exist anymore

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 
 from generate_spectrum import generate_data,wien_approximation
 from pixel_operations import choose_pixels, generate_combinations
-from temperature_functions import compute_temperature, compute_poly_temperature
+from temperature_functions import compute_poly_temperature
 from kfold import order_selection
 
 ### Emissivity functions
@@ -53,131 +53,72 @@ wl_vec = np.linspace(300,1100,(int)(3000))
 pix_vec = np.linspace(0,2999,3000)
 pix_vec = np.array(pix_vec,dtype=np.int64)
 
-### Chosen emissivity function
-chosen_eps = gr_eps
-
-### Create data
+### Chosen temperature 
 T = 3000
 
-#model_list = np.array([bb_eps,gr_eps,w_eps,art_eps])
-model_list = np.array([w_eps,w_eps,w_eps,w_eps,w_eps,w_eps,w_eps,w_eps,w_eps,w_eps])
-
+### Chosen emissivity function
+chosen_eps = gr_eps
 model_list = []
 for it in range(2):
-    model_list.append(gr_eps)
-    
+    model_list.append(chosen_eps)
+
 model_list = np.array(model_list)
 
-f,ax = plt.subplots(len(model_list),3)
+### Plots
+f,ax = plt.subplots(len(model_list),2)
 
+### Iterate over multiple models
 it = 0
-
 for f_eps in model_list:
     print("Model: ",it)
-    
+
     ### Generate some data
     I_calc,noisy_data,filtered_data,data_spl,pix_sub_vec = generate_data(
             wl_vec,T,pix_vec,f_eps)
     wl_sub_vec = wl_vec[pix_sub_vec]
-    
 
-    
-    ### Compute the temperature
-#    Tave,std,rse,refined_fit,sol = compute_temperature(data_spl,cmb_pix,pix_sub_vec,wl_vec)
-    with np.errstate(invalid='raise'):
-        poly_order = order_selection(data_spl,filtered_data,
-                           pix_sub_vec,wl_vec,
-                           bb_eps)
+    ### Choose the order of the emissivity 
+    poly_order = order_selection(data_spl,filtered_data,
+                       pix_sub_vec,wl_vec,
+                       bb_eps)
 
-        ### Pixel operations
-        chosen_pix = choose_pixels(pix_sub_vec,bin_method='average')
-        cmb_pix = generate_combinations(chosen_pix,pix_sub_vec)
-        
-        Tave,std,rse,sol = compute_poly_temperature(data_spl,cmb_pix,
-                                                    pix_sub_vec,wl_vec,
-                                                    poly_order)
-        refined_fit = False
-        if poly_order > 0:
-            refined_fit = True
-            
-        print(sol.message,sol.success)
+#    ### Pixel operations
+#    chosen_pix = choose_pixels(pix_sub_vec,bin_method='average')
+#    cmb_pix = generate_combinations(chosen_pix,pix_sub_vec)
+#
+#    Tave,std,rse,sol = compute_poly_temperature(data_spl,cmb_pix,
+#                                                pix_sub_vec,wl_vec,
+#                                                poly_order)
+#    refined_fit = False
+#    if poly_order > 0:
+#        refined_fit = True
+#
+#    print(sol.message,sol.success)
+#
+#    ### Reconstruct data
+#    bb_reconstructed = wien_approximation(wl_sub_vec,Tave,bb_eps)
+#    eps_vec_reconstructed = 10**filtered_data/bb_reconstructed
+#    # Since we get epsilon from the filtered data, "reconstructed_data" will be
+#    # exactly like "filtered_data"
+#    reconstructed_data = bb_reconstructed * eps_vec_reconstructed # exactly filtered   
+#
+#    ### Plots
+#    if it == 0:
+#        ax[it][0].set_title("Intensity")
+#        ax[it][1].set_title("Emissivity")
+#
+#    # Intensity
+#    ax[it][0].semilogy(wl_vec,noisy_data)
+#    ax[it][0].semilogy(wl_sub_vec,reconstructed_data)
+#
+#    T_string = str(round(Tave,1)) + "+/-" + str(round(std,2)) + " K"
+#    error = np.abs((Tave-T)/T)*100
+#
+#    T_string += "\n" + str(round(error,2)) + " %"
+#    ax[it][0].text(850,np.average(I_calc)/100,T_string)
+#
+#    # Emissivity
+#    ax[it][1].plot(wl_vec,f_eps(wl_vec,Tave),'--')
+#    ax[it][1].plot(wl_sub_vec,eps_vec_reconstructed) 
 
-#    # Calculate the MSE
-#    Ipred = wien_approximation(wl_sub_vec,Tave,bb_eps)
-#    pwr = 0
-#    eps_vec = np.zeros(len(wl_sub_vec))
-#    for c in sol.x:
-#        eps_vec += c * wl_sub_vec ** pwr
-#        pwr += 1
-#        
-#    Ipred *= eps_vec
-#    Ipred = np.log10(np.abs(Ipred))
-#                
-#    mse = 1/len(filtered_data) * np.sum((filtered_data - Ipred)**2)
-#    
-
-#        
-#    
-#    return Tave,std,rse,sol,mse,refined_fit
-##    print(mse_array,np.nanmean(mse_array,axis=0))
-
-    ### Reconstruct data
-    bb_reconstructed = wien_approximation(wl_sub_vec,Tave,bb_eps)
-    eps_vec_reconstructed = 10**filtered_data/bb_reconstructed
-    # Since we get epsilon from the filtered data, "reconstructed_data" will be
-    # exactly like "filtered_data"
-        
-    reconstructed_data = bb_reconstructed * eps_vec_reconstructed # exactly filtered   
-
-    reconstruct_alt = wien_approximation(wl_sub_vec,Tave,bb_eps)
-    pwr = 0
-#    eps_vec = np.zeros(len(wl_sub_vec))
-#    for c in sol.x:
-#        eps_vec += c * wl_sub_vec ** pwr
-#        pwr += 1
-    wl_min = np.min(wl_sub_vec)
-    wl_max = np.max(wl_sub_vec)
-    cheb = Chebyshev(sol.x,[wl_min,wl_max])
-    eps_vec = chebyshev.chebval(wl_sub_vec,cheb.coef)
-    
-        
-    reconstruct_alt *= eps_vec
-#    reconstruct_alt = np.log10(np.abs(reconstruct_alt))    
-    
-    ### Plots
-    if it == 0:
-        ax[it][0].set_title("Intensity")
-        ax[it][1].set_title("Emissivity")
-        
-    
-    # Intensity
-    ax[it][0].semilogy(wl_vec,noisy_data)
-    ax[it][0].semilogy(wl_sub_vec,reconstructed_data)
-    ax[it][0].semilogy(wl_sub_vec,reconstruct_alt)
-    
-    T_string = str(round(Tave,1)) + "+/-" + str(round(std,2)) + " K"
-    error = np.abs((Tave-T)/T)*100
-    
-    T_string += "\n" + str(round(error,2)) + " %"
-    ax[it][0].text(850,np.average(I_calc)/100,T_string)
-    
-    # Emissivity
-    ax[it][1].plot(wl_vec,f_eps(wl_vec,Tave),'--')
-    ax[it][1].plot(wl_sub_vec,eps_vec_reconstructed) 
-    
-    if not refined_fit:
-        # Calculate the average emissivity
-        eps_ave = np.average(eps_vec)
-        eps_std = np.std(eps_vec)
-        ax[it][1].plot(wl_vec,eps_ave*np.ones(len(wl_vec)),'-.')
-        print(eps_ave,eps_std)
-        
-    if refined_fit:
-        eps_poly = Chebyshev(sol.x,[np.min(wl_vec),np.max(wl_vec)])
-        eps_val = chebyshev.chebval(wl_vec,eps_poly.coef,'-.')
-        ax[it][1].plot(wl_vec,eps_val)
-    
-    ax[it][2].plot(wl_sub_vec,reconstructed_data / reconstruct_alt)
-
-    
     it += 1

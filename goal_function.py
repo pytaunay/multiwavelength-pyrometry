31a7f7a67320cbb3c68c10b3f4e013cbdb8a170c
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-03-19 15:57:41 -0400

    Remove Chebyshev polynomials for now and add new tukey_fencing function

--- goal_function.py
+++ goal_function.py
@@ -19,149 +19,50 @@
 
 import numpy as np
 
-from numpy.polynomial import Chebyshev, chebyshev
-
-from scipy.stats import iqr
+from numpy.polynomial import Polynomial, polynomial
 
 from spectropyrometer_constants import C2
-from generate_spectrum import wien_approximation
+from statistics import tukey_fence
 
 
-def tukey_fence(Tvec):
-    ### Exclude data w/ Tukey fencing
-    T_iqr = iqr(Tvec)
-    T_qua = np.percentile(Tvec,[25,75])
+def goal_function(poly_coeff, logR, wl_v0, wl_v1, wl_min, wl_max): 
+    '''
+    Function: goal_function
+    The function to minimize to obtain the correct temperature reading
+    Inputs:
+        - X: the coefficients for the polynomial fit representation of the
+        emissivity. 
+        - logR: the natural logarithm of the ratio of intensity at wavelength l1
+        to intensity at wavelength l0
+        - wl_v0, wl_v1: Chosen wavelengths vectors (nm)
+        - l_min, l_max: minimum and maximum wavelengths for the filtered data (nm)    
+    '''
+    # Build the higher-order coefficients for the polynomial
+    all_coeff = np.array([1])
+    all_coeff = np.append(all_coeff,poly_coeff)
     
-    min_T = T_qua[0] - 1.25*T_iqr
-    max_T = T_qua[1] + 1.25*T_iqr
-    
-    T_left = Tvec[(Tvec>min_T) & (Tvec<max_T)]
-    
-    ### Calculate standard deviation, average, standard error
-    std = np.std(T_left)
-    Tave = np.mean(T_left)
-#    rse = std/Tave*100
-    
-    rse = (T_qua[1] - T_qua[0]) / (T_qua[1] + T_qua[0])
-    
-
-    return Tave,std,rse,T_left
-
-'''
-Function: goal_function
-The function to minimize to obtain the correct temperature reading
-Inputs:
-    - X: the coefficients for the polynomial fit representation of the
-    emissivity. The basis for the fit is the Chebyshev polynomials.
-    - logR: the natural logarithm of the ratio of intensity at wavelength l1
-    to intensity at wavelength l0
-    - wl_v0, wl_v1: Chosen wavelengths vectors (nm)
-    - l_min, l_max: minimum and maximum wavelengths for the filtered data (nm)    
-'''
-def goal_function(poly_coeff,logR,wl_v0,wl_v1,wl_min,wl_max):   
     # Create a polynomial representation with the proposed coefficients
     # Rescaling is done internally by providing the bounds l_min and l_max
-    cheb = Chebyshev(poly_coeff,[wl_min,wl_max])
+    domain = np.array([wl_min,wl_max])
+    pol =  Polynomial(all_coeff,domain)
     
     # Calculate the emissivities at the corresponding wavelengths
-    eps1 = chebyshev.chebval(wl_v1,cheb.coef)
-    eps0 = chebyshev.chebval(wl_v0,cheb.coef)
+    eps1 = polynomial.polyval(wl_v1,pol.coef)
+    eps0 = polynomial.polyval(wl_v0,pol.coef)
 
     # Invert of temperature    
     with np.errstate(invalid='raise'):
         try:
-            invT = logR - 5 *np.log(wl_v1/wl_v0) - np.log(eps0/eps1)
+            invT = logR - 5*np.log(wl_v1/wl_v0) - np.log(eps0/eps1)
 
             # Temperature
             T = 1/invT
             T *= C2 * ( 1/wl_v1 - 1/wl_v0)
 
-            ret = np.std(T)
-        
-        except:
-            ret = 1e5
-    
-    if np.isnan(ret):
-        return 1e5
-    else:   
-        return ret
-
-
-
-
-'''
-Function: goal_function
-The function to minimize to obtain the correct temperature reading
-Inputs:
-    - X: the coefficients for the polynomial fit representation of the
-    emissivity. The basis for the fit is the Chebyshev polynomials.
-    - logR: the natural logarithm of the ratio of intensity at wavelength l1
-    to intensity at wavelength l0
-    - wl_v0, wl_v1: Chosen wavelengths vectors (nm)
-    - l_min, l_max: minimum and maximum wavelengths for the filtered data (nm)    
-'''
-def mixed_goal_function(poly_coeff,logR,
-                        wl_v0,wl_v1,
-                        wl_min,wl_max,
-                        wl_sub_vec,filtered_data,
-                        bb_eps):   
-    # Create a polynomial representation with the proposed coefficients
-    # Rescaling is done internally by providing the bounds l_min and l_max
-    cheb = Chebyshev(poly_coeff,[wl_min,wl_max])
-    
-    # Calculate the emissivities at the corresponding wavelengths
-    eps1 = chebyshev.chebval(wl_v1,cheb.coef)
-    eps0 = chebyshev.chebval(wl_v0,cheb.coef)
-
-    # Find temperature and reconstruct base curve  
-    with np.errstate(invalid='raise'):
-        try:
-            invT = logR - 5 * np.log(wl_v1/wl_v0) - np.log(eps0/eps1)
-
-            # Temperature
-            T = 1/invT
-            T *= C2 * ( 1/wl_v1 - 1/wl_v0)
-
-#            Tstd = np.std(T)
-#            Tave = np.mean(T)
-#            Trse = Tstd/Tave
-
-            Tave,Tstd,Trse,_ = tukey_fence(T)
-
-        
-            # Calculate base curve
-            Ipred = wien_approximation(wl_sub_vec,Tave,bb_eps)
-            eps_vec = chebyshev.chebval(wl_sub_vec,cheb.coef)
+            # Calculate the coefficient of variation
+            Tave, Tstd, Tmetric, _ = tukey_fence(T)
             
-            Ipred *= eps_vec
-            Ipred = np.log10(np.abs(Ipred))
-                        
-            # Residual sum of squares and total sum of squares
-            rss = np.sum((filtered_data - Ipred)**2)        
-            tss = np.sum((filtered_data - np.mean(filtered_data))**2)  
-            rsquared = 1 - rss/tss
-            
-            print(rsquared,Trse)
-            
-            # If rsquared is negative or zero, we have a very bad model. Penalize!
-            if rsquared < 0:
-                rsquared = 1e3 * np.abs(rsquared)       
-            elif rsquared == 0:
-                rsquared = 1e5
-                
-            if Trse < 0:
-                Trse = 1e3 * np.abs(Trse)
-                
-#            sqrt_term = np.abs(1-rsquared) + Trse
-            sqrt_term = np.abs(1-rsquared) * Trse
-#            sqrt_term = Trse
-            
-            # Mix goal: 
-            # - Rsquared should be close to one
-            # - residual square error on temperature should be close to zero
-            ret = np.sqrt(sqrt_term)
-#            ret = sqrt_term
-            
+            ret = Tmetric
             
         except:
             ret = 1e5

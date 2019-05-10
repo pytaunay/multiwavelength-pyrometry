3f05278168c8de22cddfc158a2642b4e15ed3b1e
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-05-10 18:58:29 -0400

    Remove useless functions

--- paper/that-distribution/That_distribution.py
+++ paper/that-distribution/That_distribution.py
@@ -43,8 +43,6 @@ import pixel_operations as po
 
 from statistics import tukey_fence
 
-
-
 ### Function definitions
 def wien_approximation(wl,T,f_eps):    
     '''
@@ -165,123 +163,9 @@ def generate_That_distributions(sigma_I, T0,
         
     return data
 
-def muthat_expansion(mu,sig,order):
-    '''
-    Calculates the expansion for mu_{\hat{T}} based on mu_D and sigma_D.
-    The expansion calculates the average of 1/X where X ~ N(mu_D,sigma_D^2):
-        E[1/X] \approx 1/mu * sum_{k=0}^N (sigma/mu)**2k * (2k-1)!!
-    Because 1/mu is lumped into Teq after the call to this function, it is
-    not necessary to multiply the result of the for loop by 1//mu.
-    Inputs:
-        - mu, sig: expected value and standard deviation of X
-        - order: order of the expansion
-
-    '''
-    sigomu = sig/mu
-    sigomu2 = sigomu**2
-    approx = 0
-    for k in range(order):
-        approx += sigomu2**k * factorial2(2*k-1)
-        
-    return approx
-
-
-def compute_high_order_variance(T0,sigma_I,w):  
-    '''
-    Calculates the variance from the successive Taylor expansions. We keep
-    high orders for all of the expansions.
-    Inputs:
-        - T0: true temperature
-        - sigma_I: standard variation on the measurement noise
-    '''
-    root = 'variance_calculations/'
-    cmb_pix = np.load(root+'cmb_pix.npy')
-    I_calc = np.load(root+'I_calc.npy')
-    wl_v1 = np.load(root+'wl_v1.npy')
-    wl_v0 = np.load(root+'wl_v0.npy')
-    eps0 = np.load(root+'eps0.npy')
-    eps1 = np.load(root+'eps1.npy')
-    filtered_data = np.load(root+'filtered_data.npy')
-    
-    # Number of combinations
-    Ncomb = len(wl_v1)
-    
-    ### Denominator average and variance for all wavelengths
-    # mud, sigd
-    mud = np.zeros(Ncomb)
-    for idx in range(Ncomb):
-        pixi = cmb_pix[idx,0]
-        pixj = cmb_pix[idx,1]
-        
-        # window size over 2
-        wo2 = (int)(sc.window_length/2)
-        
-#        icontrib = np.sum(np.log(I_calc)[pixi-wo2:pixi+wo2+1])
-#        jcontrib = np.sum(np.log(I_calc)[pixj-wo2:pixj+wo2+1])
-        icontrib = np.average(np.log(I_calc)[pixi-wo2:pixi+wo2+1])
-        jcontrib = np.average(np.log(I_calc)[pixj-wo2:pixj+wo2+1])
-        mud[idx] = icontrib - jcontrib
-#        mud[idx] = 1/w * (icontrib - jcontrib)
-#        mud[idx] = np.log(I_calc)[pixi] -  np.log(I_calc)[pixj] 
-#        mud[idx] = np.log(10)*(filtered_data[pixi] - filtered_data[pixj])
-    
-    mud += - 5*np.log(wl_v1/wl_v0) - np.log(eps0/eps1)
-    sigd = np.ones_like(mud) * np.sqrt(2/w)*sigma_I
-    mudmin = np.min(np.abs(mud))
-    
-    
-    
-    ratio = np.unique(sigd) / mudmin
-    ratio = np.abs(ratio)
-    
-#    print(mudmin,np.unique(sigd),ratio)
-    
-    ### muThat, sigThat
-    muThat = np.zeros_like(mud)
-    sigThat = np.zeros_like(mud)
-    
-    # Teq
-    Teq = sc.C2*(1/wl_v1-1/wl_v0) / mud
-    
-    # Taylor expansion for muThat: we want to find the "best" order in the 
-    # expansion of the mean of 1/X
-    sigomud = sigd/mud
-    sigomud2 = sigomud**2
-    
-    findN = lambda N,idx: np.abs(1/mud[idx]) * factorial2(2*N-1) * sigomud2[idx]**N
-    Narr = np.arange(0,30,1)
-    Narr = np.array(Narr,dtype=np.int64)
-    
-    for idx in range(Ncomb):
-        argmin = np.argmin(findN(Narr,idx))
-        order = (int)(Narr[argmin])
-        
-        mu = mud[idx]
-        sig = sigd[idx]
-#        muThat[idx] = muthat_expansion(mu,sig,order)
-        muThat[idx] = 1
-    
-    muThat *= Teq / T0
-    
-    # Taylor expansion for sigThat: we keep only the first two orders
-    sigThat = (Teq/T0)**2 * sigomud2 * (1 + 2*sigomud2)
-    sigThat = np.sqrt(sigThat)    
-    
-    
-    ### muTbar, sigTbar
-    # muTbar: subtract 1 bc. we want (Tbar-T0)/T0 = Tbar/T0 - 1
-    muTbar = 1/Ncomb * np.sum(muThat) - 1
-    
-    # sigTbar
-    sigTbar = 1/Ncomb**2 * np.sum(sigThat**2)
-    sigTbar = np.sqrt(sigTbar)
-    
-    return muTbar,sigTbar,ratio,muThat,sigThat
-
-
 ### Define our core case
 # Noise
-sigma_I = 0.05    
+sigma_I = 0.1
 
 # Temperature
 T0 = 3000 # K

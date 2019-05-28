d9a6cb52699f3703764124b44e242a7220a64d55
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-05-28 17:56:02 -0400

    Fix the Tukey fence call

--- goal_function.py
+++ goal_function.py
@@ -51,10 +51,10 @@ def goal_function(poly_coeff, logR, wl_v0, wl_v1, wl_min, wl_max):
 
             # Temperature
             T = 1/invT
-            T *= sc.C2 * ( 1/wl_v1 - 1/wl_v0)
+            T *= sc.C2 * (1/wl_v1 - 1/wl_v0)
 
             # Calculate the coefficient of variation
-            Tave, Tstd, Tmetric, _ = tukey_fence(T, method='dispersion')
+            Tave, Tstd, Tmetric, _ = tukey_fence(T)
             
             ret = Tmetric
             

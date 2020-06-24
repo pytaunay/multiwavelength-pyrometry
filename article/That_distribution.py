3363b295b6c25c9d9e1c6fce6f890a019659402c
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-06-24 16:03:20 -0400

    Update license and description

--- article/That_distribution.py
+++ article/That_distribution.py
@@ -1,24 +1,30 @@
-# Copyright (C) 2019 Pierre-Yves Taunay
+# MIT License
 # 
-# This program is free software: you can redistribute it andor modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright (c) 2020 Pierre-Yves Camille Regis Taunay
+#  
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 # 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 # 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https:/www.gnu.org/licenses/>.
-# 
-# Contact info: https:/github.com/pytaunay
-# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 '''
-This file contains the necessary code to generate the temperature distributions
-as shown in our 2019 paper.
+File: That_distribution.py
+
+Description: This file contains the necessary code to generate the temperature 
+distributions as shown in our 2020 RSI article (Fig. 5)
 The graph shows the effect of the test emissivity on the distribution of the
 two-color predictions of temperature. 
 We assume a correct emissivity of 0.5.

cf0912d9df2b3d4fb9dbe31955253b6eed491098
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2020-06-24 16:09:12 -0400

    Update license and description

--- article/sensitivity.py
+++ article/sensitivity.py
@@ -1,37 +1,59 @@
-# Copyright (C) 2019 Pierre-Yves Taunay
+# MIT License
 # 
-# This program is free software: you can redistribute it andor modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright (c) 2020 Pierre-Yves Camille Regis Taunay
+#  
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 # 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 # 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https:/www.gnu.org/licenses/>.
-# 
-# Contact info: https:/github.com/pytaunay
-# 
-# Source: https:/github.com/pytaunay/ILX526A
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
+'''
+File: sensitivity.py
+
+Description: perform sensitivity analysis on the computational threshold
+and the smoothing filter window length. The threshold must be changed
+by hand in the file algorithm/spectropyrometer_constants.py
+
+The results may be used to generate Fig. 10 in our 2020 RSI Journal article
+'''
 
 import numpy as np
 
-import generate_spectrum as gs
+import algorithm.generate_spectrum as gs
 
-from pixel_operations import choose_pixels, generate_combinations
-from temperature_functions import optimum_temperature
-from kfold import order_selection
+from scipy.interpolate import splrep
 
-import spectropyrometer_constant as sc
+from algorithm.pixel_operations import choose_pixels, generate_combinations
+from algorithm.temperature_functions import optimum_temperature
+from algorithm.kfold import order_selection
+
+import algorithm.spectropyrometer_constants as sc
 
 ### Emissivity functions
 # Black and gray body
 bb_eps = lambda wl,T: 1.0 * np.ones(len(wl))
-gr_eps = lambda wl,T: 0.1 * np.ones(len(wl))
+gr_eps = lambda wl,T: 0.5 * np.ones(len(wl))
+
+# Artificial tests
+art_wl = np.array([300,500,1100])
+art_eps_data = np.array([1,0.3,1])
+art_fac = np.polyfit(art_wl,art_eps_data,deg=2)
+
+a0,a1,a2 = art_fac
+art_eps = lambda wl,T: a0*wl**2 + a1*wl + a2
 
 ### Vectors of pixels and wavelengths
 wl_vec = np.linspace(400,800,(int)(3000))
@@ -42,7 +64,7 @@ pix_vec = np.array(pix_vec,dtype=np.int64)
 T = 1500
 
 ### Chosen emissivity function
-chosen_eps = gr_eps
+chosen_eps = art_eps
 
 ### Emission lines
 #el = np.array([350,400,450,500,600,650,800])
@@ -51,26 +73,80 @@ el = None
 ### Iterate over multiple models
 it = 0
 
+### Windows
+#wdw_array = np.array([3,5,7,9,11,21,31,41,51,61,71,81,91,101,201,301,401,501])
+#wdw_array = np.array([31,41,51,61,71,81,91,101,201,301,401,501])
+#wdw_array = np.array([61,71,81,91,101,201,301,401,501])
+#wdw_array = np.array([101,201,301,401,501])
+wdw_array = np.array([501])
+
+# Intensity from Wien's approximation: true data
+I_calc = gs.wien_approximation(wl_vec,T,chosen_eps)
 
 ### Generate some data
-I_calc,noisy_data,filtered_data,data_spl,pix_sub_vec = gs.generate_data(
-        wl_vec,T,pix_vec,chosen_eps,el)
-wl_sub_vec = wl_vec[pix_sub_vec]
+for wdw in wdw_array:
+
+    
+    # Add some noise and take the log of the data
+    noisy_data = np.random.normal(I_calc,0.1*I_calc)
+    log_noisy = np.log(noisy_data)
+    
+    # Remove the peaks
+    nopeak = np.copy(log_noisy)  
+    
+    # Moving average filter
+    wl = wdw
+    log_med = gs.moving_average(nopeak,wl)
+
+    
+    ### Remove the edge effects
+    if wl > 1:
+        wl_vec_sub = wl_vec[wl-1:-(wl-1)]
+        log_med = log_med[(int)((wl-1)/2):-(int)((wl-1)/2)]
+        pix_vec_sub = pix_vec[wl-1:-(wl-1)]
+    else:
+        wl_vec_sub = np.copy(wl_vec)
+        pix_vec_sub = np.copy(pix_vec)
+            
+    ### Fit a spline to access data easily
+    data_spl = splrep(wl_vec_sub,log_med)
 
 
-### Choose the order of the emissivity w/ k-fold
-poly_order = order_selection(data_spl,
-                   pix_sub_vec,wl_vec,
-                   bb_eps)
+    filtered_data = np.copy(log_med)
+    pix_sub_vec = np.copy(pix_vec_sub)
 
-### Calculate the temperature using the whole dataset
-# Pixel operations
-chosen_pix = choose_pixels(pix_sub_vec,bin_method='average')
-cmb_pix = generate_combinations(chosen_pix,pix_sub_vec)
 
-# Compute the temperature
-Tave, Tstd, Tmetric, sol = optimum_temperature(data_spl,cmb_pix,
-                                            pix_sub_vec,wl_vec,
-                                            poly_order)
+    wl_sub_vec = wl_vec[pix_sub_vec]
 
-print(sc.window_length,Tave,Tstd,np.abs(Tave-T)/T)
+    
+   
+    ntests = 1000
+    errvec = []
+    stdvec = []
+    for idx in range(ntests):
+        if np.mod(idx,ntests/10) == 0:
+            print(idx)
+        
+         ### Choose the order of the emissivity w/ k-fold
+        poly_order = order_selection(data_spl,
+                           pix_sub_vec,wl_vec,
+                           bb_eps)
+        
+        ### Calculate the temperature using the whole dataset
+        # Pixel operations
+        chosen_pix = choose_pixels(pix_sub_vec,bin_method='average')
+        cmb_pix = generate_combinations(chosen_pix,pix_sub_vec)
+        
+        # Compute the temperature
+        Tave, Tstd, Tmetric, sol = optimum_temperature(data_spl,cmb_pix,
+                                                    pix_sub_vec,wl_vec,
+                                                    poly_order)
+    
+        err = np.abs(Tave-T)/T * 100
+        
+        errvec.append(err)
+        stdvec.append(Tstd)
+    
+    errvec = np.array(errvec)
+    stdvec = np.array(stdvec)
+    print(sc.threshold, wdw, np.mean(errvec), np.mean(stdvec))
